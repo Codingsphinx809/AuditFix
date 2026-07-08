305b06f2d0bc2f54632b80cbb2ff2ff03f863f83
Codingsphinx809 <codingsphinx@duck.com>
2026-07-08 14:52:55 -0400

    Refactor AuditFixLogo component for size adjustments

--- components/AuditFixLogo.tsx
+++ components/AuditFixLogo.tsx
@@ -4,20 +4,32 @@ type AuditFixLogoProps = {
   size?: "sm" | "md" | "lg";
 };
 
-export default function AuditFixLogo({ size = "md" }: AuditFixLogoProps) {
+export default function AuditFixLogo({
+  size = "md",
+}: AuditFixLogoProps) {
   const sizes = {
-    sm: { width: 220, height: 91 },
-    md: { width: 340, height: 140 },
-    lg: { width: 460, height: 190 },
+    sm: {
+      width: 220,
+      height: 74,
+    },
+    md: {
+      width: 360,
+      height: 120,
+    },
+    lg: {
+      width: 520,
+      height: 174,
+    },
   };
 
   return (
     <Image
-      src="/logo.png"
-      alt="AuditFix - Helping dental practices turn website visitors into patients."
+      src="/AuditFixLogo.png"
+      alt="AuditFix"
       width={sizes[size].width}
       height={sizes[size].height}
       priority
+      className="h-auto w-auto"
     />
   );
 }
