728405263b8ff532401666b8fe88b589f3f4d0ee
Codingsphinx809 <codingsphinx@duck.com>
2026-07-09 09:40:04 -0400

    Update AuditFixLogo.tsx

--- components/AuditFixLogo.tsx
+++ components/AuditFixLogo.tsx
@@ -2,18 +2,25 @@ import Image from "next/image";
 
 type AuditFixLogoProps = {
   size?: "sm" | "md" | "lg";
+  variant?: "default" | "white";
 };
 
-export default function AuditFixLogo({ size = "md" }: AuditFixLogoProps) {
+export default function AuditFixLogo({
+  size = "md",
+  variant = "default",
+}: AuditFixLogoProps) {
   const sizes = {
     sm: { width: 220, height: 70 },
     md: { width: 360, height: 115 },
     lg: { width: 520, height: 166 },
   };
 
+  const src =
+    variant === "white" ? "/AuditFixLogoWhite.svg" : "/AuditFixLogo.svg";
+
   return (
     <Image
-      src="/AuditFixLogo.png"
+      src={src}
       alt="AuditFix - Helping dental practices turn website visitors into patients."
       width={sizes[size].width}
       height={sizes[size].height}
